e5d7034084ca1807fed39e7b06e33b60743b50c3
matty2d2 <matgems@googlemail.com>
2019-11-26 12:45:40 +0000

    added models

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ApplicationRecord
+  validates :name, uniqueness: {scope: :user_id}
   belongs_to :user
-  has_many :team_pokemons
+  has_many :team_pokemons, dependent: :destroy
   has_many :pokemons, through: :team_pokemons
 end
